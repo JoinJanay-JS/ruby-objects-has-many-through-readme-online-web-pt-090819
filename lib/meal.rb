b98e9f1b52826eb70e327d5120dc9cbc5bbdcc81
Janay Bailey <tjanay87@gmail.com>
2019-10-21 15:43:30 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -19,5 +19,9 @@ attr_accessor :waiter, :customer, :total, :tip
     Meal.new(self, customer, total, tip)
   end
   
-  
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
 end
